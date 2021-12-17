d3b7e9b9255f2cf3516d26b85c980d50e29b1fee
朱大卫 <1800012713@pku.edu.cn>
2021-12-17 15:57:53 +0800

    real error detection by exit(-1)

--- src/part03/main.cpp
+++ src/part03/main.cpp
@@ -11,50 +11,50 @@ extern int yylex();
 
 int main(int argc, char **argv){
 
-    // if (argc < 2) {
-    //     cout<<"Parameter Error!"<<endl;
-    //     return 1;
-    // }
+    if (argc < 2) {
+        cout<<"Parameter Error!"<<endl;
+        return 1;
+    }
 
-    // char opt;
-    // char *input_path = nullptr;
-    // char *output_path = nullptr;
-    // while( (opt = getopt(argc, argv, "o:S:")) != -1 ){
-    //         if(opt == 'S'){
-    //             input_path = optarg;
-    //         }
-    //         if(opt == 'o'){
-    //             output_path = optarg;
-    //         }
-    // }
+    char opt;
+    char *input_path = nullptr;
+    char *output_path = nullptr;
+    while( (opt = getopt(argc, argv, "o:S:")) != -1 ){
+            if(opt == 'S'){
+                input_path = optarg;
+            }
+            if(opt == 'o'){
+                output_path = optarg;
+            }
+    }
 
-    // FILE* input = fopen(input_path, "r");
-    // if(input == nullptr){
-    //     cout<<"Error, no input file!"<<endl;
-    //     return 1;
-    // } 
-    // FILE* output = fopen(output_path, "w");
-    // if(output == nullptr){
-    //     cout<<"Error, no output file!"<<endl;
-    //     return 1;
-    // }
-    // yyin = input;
-    // yyout = output;
+    FILE* input = fopen(input_path, "r");
+    if(input == nullptr){
+        cout<<"Error, no input file!"<<endl;
+        return 1;
+    } 
+    FILE* output = fopen(output_path, "w");
+    if(output == nullptr){
+        cout<<"Error, no output file!"<<endl;
+        return 1;
+    }
+    yyin = input;
+    yyout = output;
 
-    // streambuf* bak_cout = cout.rdbuf();
-    // ofstream new_cout(output_path);
-    // cout.rdbuf(new_cout.rdbuf());
+    streambuf* bak_cout = cout.rdbuf();
+    ofstream new_cout(output_path);
+    cout.rdbuf(new_cout.rdbuf());
 
     yyparse();
 
-    // for(string line: code_list){
-    //     cout << line << endl;
-    // }
+    for(string line: code_list){
+        cout << line << endl;
+    }
 
-    // //恢复cout重定向
-    // cout.rdbuf(bak_cout);
-    // fclose(input);
-    // fclose(output);
+    //恢复cout重定向
+    cout.rdbuf(bak_cout);
+    fclose(input);
+    fclose(output);
 
 
     return 0;
